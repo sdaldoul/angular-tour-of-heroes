43de3391896a3f42216a52b1b3ee7cc6b551bcbf
Slaheddine DALDOUL <daldoul.slaheddine@gmail.com>
2017-07-19 00:19:42 +0200

    services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -64,16 +64,30 @@ import { HeroService } from './hero.service';
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+  providers: [HeroService]
+
 })
 export class AppComponent {
   title = 'Tour of Heroes';
   heroes: Hero[];
   selectedHero: Hero;
 
-  heroService = new HeroService(); // don't do this
+  constructor(private heroService: HeroService) { }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+
+
+
 }
